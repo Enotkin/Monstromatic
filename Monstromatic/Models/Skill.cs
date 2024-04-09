ca9f894537e49ff8f0e8e80cb14ba7cdb6aac5c3
Kolegov Sergey <ks@smart-bilet.ru>
2024-04-09 13:31:22 +0300

    Add SkillCounter init

---
+++ Monstromatic/Models/Skill.cs
@@ -0,0 +1,32 @@
+﻿namespace Monstromatic.Models;
+
+public class Skill 
+{
+    private readonly int _featuresModificator;
+    private int _modificator = 0;
+    private int _level;
+    
+    public Skill(string name, int level, int featuresModificator)
+    {
+        Name = name;
+        _level = level;
+        _featuresModificator = featuresModificator;
+    }
+    public string Name { get; }
+    public int Value => Level + _modificator + _featuresModificator;
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            _level = value;
+            
+        }
+    }
+
+    public int SetModificator(int value)
+    {
+        _modificator = value;
+        return Value;
+    }
+}
---
+++ Monstromatic/Views/SkillCounter.axaml.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace Monstromatic.Views;
+
+public partial class SkillCounter : UserControl
+{
+    public SkillCounter()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public static readonly StyledProperty<int> Counter =
+        AvaloniaProperty.Register<SkillCounter, int>(nameof(CounterSource));
+        
+    public int CounterSource
+    {
+        get => GetValue(Counter);
+        set => SetValue(Counter, value);
+    }
+
+    private void IncreaseButtonClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void DecreaseButtonClick(object sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
