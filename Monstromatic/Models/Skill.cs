75a5f16b8524ee690060606d2c63bc601ea71a93
Enotkin <enotkin777@gmail.com>
2024-04-24 22:58:27 +0300

    Add skill counter (#1)
    Add SkillCounter

---
+++ Monstromatic/Models/Skill.cs
@@ -0,0 +1,36 @@
+﻿namespace Monstromatic.Models;
+
+public class Skill
+{
+    private const int MinValue = 1;
+    private readonly int _featuresModificator;
+    
+    public Skill(string name, int level, int featuresModificator)
+    {
+        Name = name;
+        Level = level;
+        Modificator = 0;
+        _featuresModificator = featuresModificator;
+    }
+    public string Name { get; }
+    public int Value => GetValue();
+    private int Modificator { get; set; }
+    public int Level { get; set; }
+    
+    public void Increment()
+    {
+        Modificator++;
+    }
+
+    public void Decrement()
+    {
+        if (Value - 1 >= MinValue)
+            Modificator--;
+    }
+
+    private int GetValue()
+    {
+        var value = Level + Modificator + _featuresModificator;
+        return value < MinValue ? MinValue : value;
+    }
+}
