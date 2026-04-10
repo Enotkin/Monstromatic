f3719af5b9dabd704c8df810d1141053cf2ab1fa
Kolegov Sergey <ks@smart-bilet.ru>
2026-04-10 13:13:56 +0300

    Удалил лишний код

--- Monstromatic/Views/EncounterView.axaml.cs
+++ Monstromatic/Views/EncounterView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
@@ -13,61 +14,102 @@ namespace Monstromatic.Views;
 public partial class EncounterView : ReactiveWindow<EncounterViewModel>
 {
     private bool _isExpanded = true;
-    private double _expanderHeight = 0;
+    private double _expanderHeight;
+
     public EncounterView()
     {
         InitializeComponent();
-        
+
         this.FindControl<Grid>("Header")?.AddHandler(
-            PointerPressedEvent, Header_PointerPressed,RoutingStrategies.Bubble | RoutingStrategies.Direct, true);
+            PointerPressedEvent, Header_PointerPressed, RoutingStrategies.Bubble | RoutingStrategies.Direct, true);
 
         Height = 300;
         this.AddHandler(SizeChangedEvent, WindowResized);
-        
-        CustomHeader.CloseSender += Close;
-        CustomHeader.ExpandedStateSender += ChangeExpandState;
-        CustomHeader.ChangeColorSender += ChangeBorderColor;
-        
+
         ExpandableGrid.PropertyChanged += (sender, args) =>
         {
             if (args.Property.Name == "Height")
+            {
                 UpdateWindowMeasureAsync();
+            }
         };
+
         this.MaxHeight = 0.9 * Screens.ScreenFromWindow(this)!.WorkingArea.Height;
 
-        
-        ChangeBorderColor(CustomHeader.CurrentBrush);
-        
+        FillColorSelector(GetBrushes());
+
 #if DEBUG
         this.AttachDevTools();
 #endif
-        
     }
-    
+
     protected override void OnOpened(EventArgs e)
     {
         base.OnOpened(e);
         UpdateWindowMeasureAsync();
     }
-    
-    
+
+    private static List<IBrush> GetBrushes()
+    {
+        return
+        [
+            new SolidColorBrush(0xFFA0A0A0), // grey
+            new SolidColorBrush(0xFFFFFFFF), // white
+            new SolidColorBrush(0xFFFF7F7E), // red
+            new SolidColorBrush(0xFF81C8FE), // blue
+            new SolidColorBrush(0xFFFFE87E), // yellow
+            new SolidColorBrush(0xFFFFB180), // orange
+            new SolidColorBrush(0xFFFF7FEC), // pink
+            new SolidColorBrush(0xFFA27FFF), // purple
+            new SolidColorBrush(0xFF7FFF8E) // green
+        ];
+    }
+
+    private void FillColorSelector(IEnumerable<IBrush> colors)
+    {
+        var colorSelector = this.GetControl<ComboBox>("ColorSelector");
+        colorSelector.ItemsSource = colors;
+        colorSelector.SelectedIndex = 0;
+    }
+
+    private void ColorSelectorOnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var colorSelector = this.GetControl<ComboBox>("ColorSelector");
+        var header = this.GetControl<Grid>("Header");
+
+        if (colorSelector.SelectedItem is not IBrush selectedBrush)
+        {
+            return;
+        }
+
+        header.Background = selectedBrush;
+        ChangeBorderColor(selectedBrush);
+    }
+
     private void ChangeBorderColor(IBrush brush)
     {
         var border = this.GetControl<Border>("Border");
         border.BorderBrush = brush;
     }
 
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
     private void Header_PointerPressed(object sender, PointerPressedEventArgs e)
     {
-        if (e.Source is Grid {Name: "Header"})
+        if (e.Source is Grid { Name: "Header" })
+        {
             BeginMoveDrag(e);
+        }
     }
 
     private void WindowResized(object sender, SizeChangedEventArgs e)
     {
         ExpandableGrid.Height += e.NewSize.Height - e.PreviousSize.Height;
     }
-    
+
     private void ChangeExpandState(bool state)
     {
         if (_isExpanded)
@@ -81,10 +123,10 @@ public partial class EncounterView : ReactiveWindow<EncounterViewModel>
         }
 
         _isExpanded = !_isExpanded;
-        
+
         UpdateWindowMeasureAsync();
     }
-    
+
     private async void UpdateWindowMeasureAsync()
     {
         await Task.Delay(100);
